751e26bf9109ab57951a5e85be6749e033cbc0b6
Yusuf Sönmez <Yusufsonmez951@gmail.com>
2023-12-19 01:11:50 +0300

    Bigbox studio

---
+++ js/custom/extra.js
@@ -0,0 +1,30 @@
+// navbar transaction
+window.addEventListener("DOMContentLoaded", (event) => {
+  var navbarMobile = function () {
+    const nCollapsible = document.body.querySelector("#mainNavbar");
+    if (!nCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      nCollapsible.classList.remove("navbar-mobile");
+    } else {
+      nCollapsible.classList.add("navbar-mobile");
+    }
+  };
+  navbarMobile();
+  document.addEventListener("scroll", navbarMobile);
+  const myNavbar = document.body.querySelector("#mainNavbar");
+  if (myNavbar) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNavbar",
+      offset: 74,
+    });
+  }
+});
+// canvas oto close
+var BtnCanvas = document.querySelectorAll(".btn-close-canvas");
+for (let i = 0; i < BtnCanvas.length; i++) {
+  BtnCanvas[i].addEventListener("click", function () {
+    document.querySelector('[data-bs-dismiss = "offcanvas"]').click();
+  });
+}
